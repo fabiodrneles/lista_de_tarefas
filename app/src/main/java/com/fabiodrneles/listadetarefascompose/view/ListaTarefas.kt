1f912593d82682c850cf6d49a88195732c830982
Fabio Dorneles <fabio.dorneles@stone.com.br>
2024-04-12 18:26:45 -0300

    adicionando botão para adicionar tarefa, estilizando as telas, trocando propriedades pois algumas estão em fase de testes

--- app/src/main/java/com/fabiodrneles/listadetarefascompose/view/ListaTarefas.kt
+++ app/src/main/java/com/fabiodrneles/listadetarefascompose/view/ListaTarefas.kt
@@ -1,13 +1,70 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.fabiodrneles.listadetarefascompose.view
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.fabiodrneles.listadetarefascompose.R
+import com.fabiodrneles.listadetarefascompose.ui.theme.BLACK
+import com.fabiodrneles.listadetarefascompose.ui.theme.Purple700
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ListaTarefas(
     navController: NavController
-){
-    Text(text = "listaTarefa")
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+
+                title = {
+                    Text(
+                        text = "Lista de Tarefas",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Purple700)
+            )
+        },
+        containerColor = BLACK,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                          navController.navigate("salvarTarefa")
+
+            },
+                containerColor = Blue,
+                shape = CircleShape
+        ) {
+                Image(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
+                    contentDescription = "Ícone de salvar tarefa!"
+                )
+
+            }
+        }
+    ) {
+
+    }
 
 }
+    
--- app/src/main/java/com/fabiodrneles/listadetarefascompose/view/SalvarTarefa.kt
+++ app/src/main/java/com/fabiodrneles/listadetarefascompose/view/SalvarTarefa.kt
@@ -1,11 +1,49 @@
 package com.fabiodrneles.listadetarefascompose.view
 
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.core.content.contentValuesOf
 import androidx.navigation.NavController
+import com.fabiodrneles.listadetarefascompose.ui.theme.BLACK
+import com.fabiodrneles.listadetarefascompose.ui.theme.Purple700
+import com.fabiodrneles.listadetarefascompose.ui.theme.WHITE
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SalvarTarefa(
     navController: NavController
 ){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+
+                title = {
+                    Text(
+                        text = "Salvar Tarefa",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Purple700)
+            )
+        },
+        containerColor = BLACK
+
+    ){
+
+
+
+    }
 
 }
