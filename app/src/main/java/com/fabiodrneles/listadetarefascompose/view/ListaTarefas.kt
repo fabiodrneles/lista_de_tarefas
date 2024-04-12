9692f1657af416d34143157489e486c01133c616
Fabio Dorneles <fabio.dorneles@stone.com.br>
2024-04-12 13:34:05 -0300

    teste de resposta da config de navigation

--- app/src/main/java/com/fabiodrneles/listadetarefascompose/view/ListaTarefas.kt
+++ app/src/main/java/com/fabiodrneles/listadetarefascompose/view/ListaTarefas.kt
@@ -1,5 +1,6 @@
 package com.fabiodrneles.listadetarefascompose.view
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
@@ -7,5 +8,6 @@ import androidx.navigation.NavController
 fun ListaTarefas(
     navController: NavController
 ){
+    Text(text = "listaTarefa")
 
 }
